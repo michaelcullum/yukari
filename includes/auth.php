d80e594a301e356737785639e9626fa34bac6186
Obsidian <Obsidian@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-07-19 14:36:47 +0000

    Some more work on the auths system
    git-svn-id: http://my-svn.assembla.com/svn/failnet/trunk@133 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- includes/auth.php
+++ includes/auth.php
@@ -16,6 +16,11 @@
  */
 
 // @todo Rewrite with plugin!
+// @todo Delete user
+// @todo Delete hostmasks from access list
+// @todo Add hostmask to access list
+// @todo View access list for a user
+// @todo Link users?
 
 /**
  * This program is free software; you can redistribute it and/or modify
@@ -49,9 +54,6 @@ if(!defined('IN_FAILNET')) exit(1);
  */
 class failnet_auth extends failnet_common
 {
-	// Authed users.
-	public $users = array();
-	
 	/**
 	 * phpass hashing handler
 	 * @var object
@@ -67,27 +69,6 @@ class failnet_auth extends failnet_common
 	{
 		display('=== Loading Failnet password hashing system');
 			$this->hash = new failnet_hash(8, false);
-		display('=== Loading user database'); 
-			$this->load();
-	}
-	
-	/**
-	 * Method to (re)load the users database.
-	 * @return void
-	 */
-	public function load()
-	{
-		$this->users = file(FAILNET_ROOT . 'data/users', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-		foreach ($this->users as &$user)
-		{
-			$user_row = explode('::', rtrim($user));
-			$user['nick'] = array_shift($user_row);
-			$user['level'] = array_shift($user_row);
-			$user['hash'] = array_shift($user_row);
-			$user['authed'] = false;
-			$user['hosts'] = (!empty($user_row)) ? $user_row : array();
-			$user['regex'] = hostmasks_to_regex($user['hosts']);
-		}
 	}
 	
 	/**
@@ -96,24 +77,28 @@ class failnet_auth extends failnet_common
 	 * @param string $password - The password the sender specified.
 	 * @return mixed - True if password is correct, false if password is wrong, NULL if no such user.
 	 */
-	public function auth($sender, $password)
+	public function auth($hostmask, $password)
 	{
-		foreach ($this->users as &$user)
+		// First, we want to parse the user's hostmask here.
+		parse_hostmask($hostmask, $nick, $user, $host);
+		
+		// Now, let's do a query to grab the row for that user
+		$this->failnet->sql('users', 'get')->execute(array(':nick' => $nick));
+		$result = $this->failnet->sql('users', 'get')->fetch(PDO::FETCH_ASSOC); 
+		
+		// No such user?  Derr...
+		if(!$result)
+			return NULL;
+		
+		// Let's check that password now...
+		if($this->hash->check($password, $result['hash']))
 		{
-			if ($user['nick'] == strtolower($sender))
-			{
-				if ($this->hash->check($password, $user['hash']))
-				{
-					$user['authed'] = true;
-					return true;
-				}
-				else
-				{
-					return false;
-				}
-			}
+			// Success!  We need to just add a row to the sessions table now so that the login persists.
+			return $this->failnet->sql('sessions', 'create')->execute(array(':key' => $this->failnet->unique_id(), ':user' => $result['user_id'], ':time' => time(), ':hostmask' => $hostmask));
 		}
-		return NULL;
+		
+		// FAIL!  NOW GIT OUT OF MAH KITCHEN!
+		return false;
 	}
 
 	/**
@@ -162,10 +147,10 @@ class failnet_auth extends failnet_common
 	 * Looks up the authorization level for a certain user...
 	 * @param string $nick - The user to check for.
 	 * @param string $hostmask - The hostmask for the user we're checking, if we want to use access lists for this.
-	 * @return mixed - Always returns 100 if boolean false is used as the authlevel, or if no such user NULL is returned.
+	 * @return mixed - Always returns 100 if boolean false is used as the authlevel, integer for the authlevel if in the access list or logged in, or false if the user isn't logged in/does not exist.
 	 */
 	public function authlevel($nick, $hostmask = false)
-	{
+	{	// @todo Check if this works.  o_O
 		if($nick === false)
 			return 100;
 
@@ -174,21 +159,32 @@ class failnet_auth extends failnet_common
 
 		if(empty($hostmask))
 		{
-			// @todo CHECK FOR LOGGED IN USER!
-			$this->failnet->sql('users', 'get_level')->execute(array(':nick' => $nick));
-			$result = $this->failnet->sql('users', 'get_level')->fetch(PDO::FETCH_ASSOC);
+			$sql = $this->failnet->db->query('SELECT u.authlevel, s.login_time
+				FROM sessions s, users u 
+				WHERE u.user_id = s.user_id
+					AND LOWER(u.nick) = LOWER(' . $this->failnet->db->quote($nick) . ') 
+				ORDER BY s.login_time DESC');
+			
+			// Do we have a logged in user with that nick?
+			$result = $sql->fetch(PDO::FETCH_ASSOC);
 			if(!$result)
-			{
 				return false;
-			}
-			else
-			{
-				return $result['authlevel'];
-			}
+			
+			return $result['authlevel'];
 		}
 		else
 		{
-			// @todo Write a multi-query here to first check to see if the user with $nick is on the access list, and if so, THEN get the authlevel for them.
+			$sql = $this->failnet->db->query('SELECT u.authlevel
+				FROM access a, users u 
+				WHERE u.user_id = a.user_id
+					AND LOWER(a.hostmask) = LOWER(' . $this->failnet->db->quote($hostmask) . ')');
+			
+			// Do we have a user with that hostmask assigned or not?
+			$result = $sql->fetch(PDO::FETCH_ASSOC);
+			if(!$result)
+				return false;
+			
+			return $result['authlevel'];
 		}
 	}
 	
@@ -204,8 +200,7 @@ class failnet_auth extends failnet_common
 		$user_exists = $this->failnet->db->query('SELECT COUNT(*) FROM users WHERE nick = ' . $this->failnet->db->quote($nick))->fetchColumn();
 		if(!$user_exists)
 		{
-			$this->failnet->sql('users', 'create')->execute(array(':nick' => $nick, ':authlevel' => $authlevel, ':hash' => $this->hash->hash($password)));
-			return true;
+			return $this->failnet->sql('users', 'create')->execute(array(':nick' => $nick, ':authlevel' => $authlevel, ':hash' => $this->hash->hash($password)));
 		}
 		else
 		{
--- includes/core.php
+++ includes/core.php
@@ -16,7 +16,6 @@
  */
 
 // @todo failnet_core::no_factoid() method, for saying something when there's no factoid available for that.
-// @todo unique_id() function for creating session keys for auth system
 
 /**
  * This program is free software; you can redistribute it and/or modify
@@ -165,9 +164,9 @@ class failnet_core
 				// Config table...
 				$this->db->query(file_get_contents(FAILNET_ROOT . 'includes/schemas/config.sql'));
 				display(' -  Creating users table...');
-				$this->db->query(file_get_contents(FAILNET_ROOT . 'includes/schemas/session.sql'));
-				display(' -  Creating sessions table...');
 				$this->db->query(file_get_contents(FAILNET_ROOT . 'includes/schemas/users.sql'));
+				display(' -  Creating sessions table...');
+				$this->db->query(file_get_contents(FAILNET_ROOT . 'includes/schemas/sessions.sql'));
 				display(' -  Creating access table...');
 				$this->db->query(file_get_contents(FAILNET_ROOT . 'includes/schemas/access.sql'));
 				display(' -  Creating ignored hostmasks table...');
@@ -178,7 +177,7 @@ class failnet_core
 			// Now, we need to build our default statements.
 			// Config table
 			$this->build_sql('config', 'create', 'INSERT INTO config ( name, value ) VALUES ( :name, :value )');
-			$this->build_sql('config', 'get', 'SELECT value, ROWID id FROM config WHERE LOWER(name) = LOWER(:name) LIMIT 1');
+			$this->build_sql('config', 'get', 'SELECT value FROM config WHERE LOWER(name) = LOWER(:name) LIMIT 1');
 			$this->build_sql('config', 'update', 'UPDATE config SET value = :value WHERE LOWER(name) = LOWER(:name)');
 			$this->build_sql('config', 'delete', 'DELETE FROM config WHERE name = :name');
 
@@ -187,27 +186,31 @@ class failnet_core
 			$this->build_sql('users', 'set_level', 'UPDATE users SET authlevel = :authlevel WHERE LOWER(nick) = LOWER(:nick)');
 			$this->build_sql('users', 'set_confirm', 'UPDATE users SET confirm_key = :confirm WHERE LOWER(nick) = LOWER(:nick)');
 			$this->build_sql('users', 'set_pass', 'UPDATE users SET password = :hash WHERE LOWER(nick) = LOWER(:nick)');
-			$this->build_sql('users', 'get', 'SELECT user_id, nick, authlevel, confirm_key, hash, ROWID id FROM users WHERE LOWER(nick) = LOWER(:nick) LIMIT 1');
-			$this->build_sql('users', 'get_level', 'SELECT authlevel, ROWID id FROM users WHERE LOWER(nick) = LOWER(:nick) LIMIT 1');
-			$this->build_sql('users', 'get_confirm', 'SELECT confirm_key, ROWID id FROM users WHERE LOWER(nick) = LOWER(:nick) LIMIT 1');
+			$this->build_sql('users', 'get', 'SELECT user_id, nick, authlevel, confirm_key, hash FROM users WHERE LOWER(nick) = LOWER(:nick) LIMIT 1');
+			$this->build_sql('users', 'get_level', 'SELECT authlevel FROM users WHERE LOWER(nick) = LOWER(:nick) LIMIT 1');
+			$this->build_sql('users', 'get_confirm', 'SELECT confirm_key FROM users WHERE LOWER(nick) = LOWER(:nick) LIMIT 1');
 			$this->build_sql('users', 'delete', 'DELETE FROM users WHERE nick = :nick');
 			
 			// Sessions table
 			// @todo Sessions table prepared PDO statements
-			
+			$this->build_sql('sessions', 'create', 'INSERT INTO sessions ( key_id, user_id, login_time, hostmask ) VALUES ( :key, :user, :time, :hostmask )');
+			$this->build_sql('sessions', 'delete_key', 'DELETE FROM sessions WHERE key_id = :key');
+			$this->build_sql('sessions', 'delete_user', 'DELETE FROM sessions WHERE user_id = :user');
+			$this->build_sql('sessions', 'delete_old', 'DELETE FROM sessions WHERE login_time < :time');
+			$this->build_sql('sessions', 'delete', 'DELETE FROM sessions WHERE LOWER(hostmask) = LOWER(:hostmask)');
 			
 
 			// Access list table
 			$this->build_sql('access', 'create', 'INSERT INTO access ( user_id, hostmask ) VALUES ( :user_id, :hostmask )');
 			$this->build_sql('access', 'delete', 'DELETE FROM access WHERE (user_id = :user_id AND hostmask = :hostmask )');
 			$this->build_sql('access', 'wipe', 'DELETE FROM access WHERE user_id = :user_id');
-			$this->build_sql('access', 'get', 'SELECT hostmask, ROWID id FROM access WHERE user_id = :user_id');
+			$this->build_sql('access', 'get', 'SELECT hostmask FROM access WHERE user_id = :user_id');
 			
 			// Ignored hostmasks table
 			$this->build_sql('ignore', 'create', 'INSERT INTO ignore ( ignore_date, hostmask ) VALUES ( :timestamp, :hostmask )');
-			$this->build_sql('ignore', 'delete', 'DELETE FROM ignore WHERE hostmask = :hostmask');
-			$this->build_sql('ignore', 'get_single', 'SELECT ignore_date, hostmask, ROWID id FROM ignore WHERE LOWER(hostmask) = LOWER(:hostmask) LIMIT 1');
-			$this->build_sql('ignore', 'get', 'SELECT hostmask, ROWID id from ignore');
+			$this->build_sql('ignore', 'delete', 'DELETE FROM ignore WHERE LOWER(hostmask) = LOWER(:hostmask)');
+			$this->build_sql('ignore', 'get_single', 'SELECT ignore_date, hostmask FROM ignore WHERE LOWER(hostmask) = LOWER(:hostmask) LIMIT 1');
+			$this->build_sql('ignore', 'get', 'SELECT hostmask from ignore');
 			
 			// Commit the results
 			$this->db->commit();
@@ -477,6 +480,32 @@ class failnet_core
 	public function build_sql($table, $type, $statement)
 	{
 		$this->statements[$table][$type] = $this->db->prepare($statement);
+	}
+	
+	/**
+	* Return unique id
+	* @param string $extra additional entropy
+	* @return string - The unique ID
+	* 
+	* @author (c) 2007 phpBB Group 
+	*/
+	public function unique_id($extra = 'c')
+	{
+		static $dss_seeded = false;
+		
+		$rand_seed = $this->sql('config', 'get')->execute(array(':name' => 'rand_seed'));
+		$last_rand_seed = $this->sql('config', 'get')->execute(array(':name' => 'last_rand_seed'));
+		$val = md5($rand_seed . microtime());
+		$rand_seed = md5($rand_seed . $val . $extra);
+	
+		if ($dss_seeded !== true && ($last_rand_seed < time() - rand(1,10)))
+		{
+			$this->sql('config', 'get')->execute(array(':name' => 'rand_seed', ':value' => $rand_seed));
+			$this->sql('config', 'update')->execute(array(':name' => 'last_rand_seed', ':value' => time()));
+			$dss_seeded = true;
+		}
+	
+		return substr($val, 4, 16);
 	}
 
 	/**
