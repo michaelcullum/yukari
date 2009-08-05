f1fb561de95af5fd1c1acc105769e21dd02256bc
Obsidian <Obsidian@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-08-05 17:38:11 +0000

    Now, we'll actually IGNORE the users. :D
    git-svn-id: http://my-svn.assembla.com/svn/failnet/trunk@182 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- includes/core.php
+++ includes/core.php
@@ -335,6 +335,10 @@ class failnet_core
 				}
 			}
 
+			// Check to see if the user that generated the event is ignored.
+			if($eventtype != 'response' && $this->ignore->ignored($event->gethostmask))
+				continue;
+
 			// For each plugin, we provide the event encountered so that the plugins can react to them for us  
 			foreach ($this->plugins as $name => $plugin)
 			{
