97fae52f7132cc68e4f3e42e4e535bee0430f145
Obsidian <Obsidian@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-08-11 23:27:30 +0000

    Cleaned up version of the .sh script for Failnet, thanks to Techie-Micheal.  :D
    git-svn-id: http://my-svn.assembla.com/svn/failnet/trunk@240 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- failnet.sh
+++ failnet.sh
@@ -1,4 +1,44 @@
 #!/bin/bash
-while php failnet.php;
-do true;
-done;
+
+#
+#===================================================================
+#
+#  Failnet -- PHP-based IRC Bot
+#-------------------------------------------------------------------
+# Script info:
+# Version:             2.0.0
+# SVN ID:              $Id$
+# Copyright:		   (c) 2009 - Failnet Project
+# License:             http://opensource.org/licenses/gpl-2.0.php  |  GNU Public License v2
+#
+#===================================================================
+#
+# Thanks to Techie-Micheal for writing the fancy bash script for Failnet 2.
+#
+
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License,
+# or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://opensource.org/licenses/gpl-2.0.php>.
+#
+
+# This is what server configuration file you want Failnet to load.
+SERVER="config"
+
+# Run Failnet!
+while php failnet.php $SERVER;
+	do true
+done
+
+# Uncomment this (remove the #) to have the command prompt window pause after Failnet's termination.  
+# Useful for trapping errors.
+read -s -n 1 -p "Press any key to continue ..."
