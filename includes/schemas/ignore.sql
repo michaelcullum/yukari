7ca4e385eb06507daafd1bf4269c856889ae579e
Obsidian <Obsidian@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-08-11 13:26:04 +0000

    Fix some SQL there.
    git-svn-id: http://my-svn.assembla.com/svn/failnet/trunk@210 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- includes/schemas/ignore.sql
+++ includes/schemas/ignore.sql
@@ -4,6 +4,6 @@
  */
 
 CREATE TABLE ignore (
-	ignore_date INTEGER UNSIGNED NOT NULL DEFAULT '0',
+	ignore_date INTEGER UNSIGNED NOT NULL DEFAULT 0,
 	hostmask TEXT NOT NULL DEFAULT '',
 );
--- includes/schemas/users.sql
+++ includes/schemas/users.sql
@@ -6,8 +6,8 @@
 CREATE TABLE users (
 	user_id INTEGER PRIMARY KEY NOT NULL,
 	nick TEXT NOT NULL DEFAULT '',
-	authlevel INTEGER NOT NULL DEFAULT '0',
+	authlevel INTEGER NOT NULL DEFAULT 0,
 	confirm_key TEXT NOT NULL DEFAULT '',
-	password TEXT UNSIGNED NOT NULL DEFAULT '',
+	password TEXT UNSIGNED NOT NULL DEFAULT ''
 );
 CREATE UNIQUE INDEX unique_nick ON users ( nick ) ;
