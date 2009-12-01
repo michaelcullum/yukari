782711fffa7637bbc704a92548d180d3c7f9f971
Obsidian <stratosphere.programming@gmail.com>
2009-12-01 13:26:47 -0500

    NOW QUIT KARMA SPAMMING YOU NUBS

--- includes/plugin/karma.php
+++ includes/plugin/karma.php
@@ -46,7 +46,7 @@ class failnet_plugin_karma extends failnet_plugin_common
 		// Check for karma changes first
 		$text = $this->event->get_arg('text');
 		$sender = $this->event->nick;
-		if($this->failnet->karma->check_word($text))
+		if($this->event->fromchannel() === true && $this->failnet->karma->check_word($text))
 		{
 			$term = strtolower(trim($text));
 			$karma_type = substr($term, -2, 2);
