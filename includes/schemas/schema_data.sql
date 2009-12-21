f8e6b71c2d508110c79ba42e3533c94d8292b737
Obsidian <stratosphere.programming@gmail.com>
2009-12-20 22:11:32 -0500

    That was moved to the config file for less stupidity.  o.O

--- includes/schemas/schema_data.sql
+++ includes/schemas/schema_data.sql
@@ -33,7 +33,6 @@
  */
 INSERT INTO config (name, value) VALUES ('rand_seed', 0);
 INSERT INTO config (name, value) VALUES ('last_rand_seed', 0);
-INSERT INTO config (name, value) VALUES ('cmd_prefix', '|');
 INSERT INTO config (name, value) VALUES ('join_on_invite', '0');
 INSERT INTO config (name, value) VALUES ('autorejoin', '0');
 INSERT INTO config (name, value) VALUES ('announce_new_tweets', '0');
