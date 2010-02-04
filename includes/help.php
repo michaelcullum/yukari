3075a6ff47183a28ab8e1f7a8cd29b9c0f69550c
Obsidian <stratosphere.programming@gmail.com>
2010-02-04 01:04:50 -0500

    and here is our beloved dynamic system, yet untested

---
+++ includes/help.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ *
+ *===================================================================
+ *
+ *  Failnet -- PHP-based IRC Bot
+ *-------------------------------------------------------------------
+ *	Script info:
+ * Version:		2.0.0 Alpha 1
+ * Copyright:	(c) 2009 - 2010 -- Failnet Project
+ * License:		GNU General Public License - Version 2
+ *
+ *===================================================================
+ *
+ */
+
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://opensource.org/licenses/gpl-2.0.php>.
+ */
+
+
+/**
+ * Failnet - Help class,
+ * 		Failnet's handler for the dynamic help system
+ *
+ *
+ * @package nodes
+ * @author Obsidian
+ * @copyright (c) 2009 - 2010 -- Failnet Project
+ * @license GNU General Public License - Version 2
+ */
+class failnet_help extends failnet_common
+{
+	private $index = array();
+
+	private $commands = array();
+
+	/**
+	 * Specialized init function to allow class construction to be easier.
+	 * @see includes/failnet_common#init()
+	 * @return void
+	 */
+	public function init() { }
+
+	public function collect($name, $commands)
+	{
+		$commands_index = array();
+		foreach($commands as $c_name => $c_value)
+		{
+			$commands_index[] = $c_name;
+			if(isset($this->commands[$c_name]))
+			{
+				$this->failnet->ui->ui_debug('Duplicate command name within dynamic help system detected');
+			}
+			$this->commands[$c_name][] = $c_value;
+		}
+		$this->index[$name] = $commands_index;
+	}
+}
---
+++ includes/plugin/help.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ *
+ *===================================================================
+ *
+ *  Failnet -- PHP-based IRC bot
+ *-------------------------------------------------------------------
+ *	Script info:
+ * Version:		2.0.0 Alpha 1
+ * Copyright:	(c) 2009 - 2010 -- Failnet Project
+ * License:		GNU General Public License - Version 2
+ *
+ *===================================================================
+ *
+ */
+
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://opensource.org/licenses/gpl-2.0.php>.
+ */
+
+
+/**
+ * Failnet - Help plugin,
+ * 		Aids users in learning to control Failnet.
+ *
+ *
+ * @package plugins
+ * @author Obsidian
+ * @copyright (c) 2009 - 2010 -- Failnet Project
+ * @license GNU General Public License - Version 2
+ */
+class failnet_plugin_help extends failnet_plugin_common
+{
+	public function help(&$name, &$commands)
+	{
+		$name = 'help';
+		$commands = array(
+			'help-p'		=> 'help-p - (no auth) - Lists the plugins with help entries available',
+			'help-c'		=> 'help-c {$plugin} - (no auth) - Lists the commands with help entries available under the specified plugin',
+			'help'			=> 'help {$command} - (no auth) - Returns the help entry for the specified command, if there is one available',
+		);
+	}
+
+	public function cmd_privmsg()
+	{
+		// Process the command
+		$text = $this->event->get_arg('text');
+		if(!$this->prefix($text))
+			return;
+
+		$cmd = $this->purify($text);
+		$this->set_msg_args(($this->failnet->config('speak')) ? $this->event->source() : $this->event->hostmask->nick);
+
+		$sender = $this->event->hostmask->nick;
+		$hostmask = $this->event->hostmask;
+		switch ($cmd)
+		{
+			case 'help-p':
+				$this->msg('Plugins with commands: ' . implode(', ', array_keys($this->failnet->help->index)));
+			break;
+
+			case 'help-c':
+				if($text !== false)
+				{
+					$this->msg((isset($this->failnet->help->index[$text])) ? 'Commands available in plugin: ' . implode(', ' . $this->failnet->help->index[$text]) : 'Invalid plugin specified.');
+				}
+				else
+				{
+					$this->msg('Please specify a plugin for retrieval of command help documentation');
+				}
+			break;
+
+			case 'help':
+				if($text !== false)
+				{
+					$this->msg((isset($this->failnet->help->commands[$text])) ? 'Command documentation: ' . implode(', ' . $this->failnet->help->commands[$text]) : 'Invalid command specified.');
+				}
+				else
+				{
+					$this->msg('Please specify a command for retrieval of command help documentation');
+				}
+			break;
+		}
+}
