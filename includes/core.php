8611d8d10da25e94837467ac43f2c56890ee1f2a
Obsidian <stratosphere.programming@gmail.com>
2010-02-03 19:18:40 -0500

    ...

--- includes/core.php
+++ includes/core.php
@@ -636,7 +636,7 @@ class failnet_core
 		}
 		else
 		{
-			$trace = dump_backtrace();
+			$trace = debug_backtrace();
 			trigger_error('Undefined property via __get(): ' . $name . ' in ' . $trace[0]['file'] .' on line ' . $trace[0]['line'] . '--', E_USER_WARNING);
 		}
 	}
