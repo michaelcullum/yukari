e66152cd68bbcea90f81817cf2bf41f4e9704357
Obsidian <stratosphere.programming@gmail.com>
2010-02-27 15:50:20 -0600

    some commenting

--- includes/constants.php
+++ includes/constants.php
@@ -31,6 +31,7 @@
 // Version
 define('FAILNET_VERSION', '2.0.0A2');
 
+// Output levels
 define('OUTPUT_SILENT', 0);
 define('OUTPUT_NORMAL', 1);
 define('OUTPUT_DEBUG', 2);
@@ -38,6 +39,7 @@ define('OUTPUT_DEBUG_FULL', 3);
 define('OUTPUT_RAW', 4);
 define('OUTPUT_SPAM', 4); // ;D
 
+// @depreciated
 define('MSG_NORMAL', 0);
 define('MSG_DEBUG', 2);
 define('MSG_DEBUG_EXTRA', 3);
