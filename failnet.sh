d4c9e1ee7a19303c010c9356c1b7b5d0f29980c5
obsidian <stratosphere.programming@gmail.com>
2010-05-07 10:28:10 -0500

    fixing the shell script headers

--- failnet.sh
+++ failnet.sh
@@ -5,7 +5,6 @@
 #
 #  Failnet -- PHP-based IRC Bot
 #-------------------------------------------------------------------
-# Script info:
 # Version:		2.1.0 DEV
 # Copyright:	(c) 2009 - 2010 -- Failnet Project
 # License:		GNU General Public License - Version 2
