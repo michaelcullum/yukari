0f9a3f9503befc8b58dd7daf7bd110795762cf8a
obsidian <stratosphere.programming@gmail.com>
2010-05-19 22:45:44 -0500

    beginning work on colors in Failnet's output

--- includes/core/ui.php
+++ includes/core/ui.php
@@ -29,6 +29,9 @@
  *
  */
 
+namespace Failnet\Core;
+use Failnet;
+
 
 /**
  * Failnet - Terminal UI class,
@@ -41,17 +44,76 @@
  * @license		http://opensource.org/licenses/gpl-2.0.php GNU GPL v2
  * @link		http://github.com/Obsidian1510/Failnet-PHP-IRC-Bot
  */
-class failnet_ui extends failnet_common
+class UI extends Common
 {
-	/**
-	 * @var string - Buffer of the stuff we are going to process
-	 */
-	public $buffer = '';
-
 	/**
 	 * @var integer - Our current output level
 	 */
 	public $output_level = 0;
+
+	/**
+	 * @var array - Various color codes for use with terminals that support it.
+	 */
+	protected $fg_colors = array();
+
+	/**
+	 * @var array - Various color codes for use with terminals that support it.
+	 */
+	protected $bg_colors = array();
+
+	/**
+	 * @var array - Array of preset color profiles for use with the UI.
+	 */
+	protected $color_profiles = array();
+
+	/**
+	 * @var boolean - Do we want to enable the use of colors in our output?
+	 */
+	public $enable_colors = false;
+
+	public function __construct()
+	{
+		if(Bot::core()->config('enable_colors') && $this->checkColorSupport())
+		{
+			$this->fg_colors = array('black' => '0;30', 'blue' => '0;34', 'green' => '0;32', 'cyan' => '0;36', 'red' => '0;31', 'purple' => '0;35', 'brown' => '0;33', 'yellow' => '1;33', 'white' => '1;37');
+			$this->bg_colors = array('black' => '40', 'red' => '41', 'green' => '42', 'yellow' => '43', 'blue' => '44', 'magenta' => '45', 'cyan' => '46', 'light_gray' => '47');
+			$this->color_profiles = array(
+				'WARNING'	=> array('background' => 'yellow', 'foreground' => 'black', 'bold' => true),
+				'ERROR'		=> array('background' => 'red', 'foreground' => 'white', 'bold' => true),
+			);
+			$this->enable_colors = true;
+		}
+
+	}
+
+	/**
+	 * Check if ANSI colors can be used.
+	 * @return boolean - Does our environment support use of colors in output?
+	 */
+	protected function checkColorSupport()
+	{
+		return ((stristr(PHP_OS, 'WIN')) ? @getenv('ANSICON') !== false : function_exists('posix_isatty') && @posix_isatty(STDOUT));
+	}
+
+	/**
+	 * Colorizes the given text
+	 * @param string $string - The string to colorizate.
+	 * @param string $profile - Name of the color profile to use on the given string.
+	 * @return string - The colorizered string.
+	 * @note "typos" intentional.
+	 */
+	public function addColor($string, $profile)
+	{
+		if(!isset($this->color_profiles[strtoupper($profile)]) || empty($this->color_profiles[strtoupper($profile)]))
+			return $string;
+
+		$profile = $this->color_profiles[strtoupper($profile)];
+
+		$codes = (isset($profile['foreground']) ? $this->fg_colors[$profile['foreground']] . ';' : '');
+		$codes .= (isset($profile['background']) ? $this->bg_colors[$profile['background']] . ';' : '');
+		$codes .= (isset($profile['bold']) ? '1;' : '');
+		return "\033[{$codes}m{$string}\033[0m";
+	}
 
 	/**
 	 * Method that handles output of all data for the UI.
