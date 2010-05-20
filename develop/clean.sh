70c0b2d98773088425b5a3f3908fd2b00c30fc9a
obsidian <stratosphere.programming@gmail.com>
2010-05-20 00:44:27 -0500

    get rid of these old files

--- develop/clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# clean up after a build
-#
-# (c) 2010 eviL3
-# obtained at http://github.com/evil3/phpbb-github_profile_link/blob/master/develop/clean.sh
-# -- no license specified within file, however project's license is GPLv2, so GPLv2 is assumed
-
-rm -f failnet-*.zip
-rm -rf failnet
