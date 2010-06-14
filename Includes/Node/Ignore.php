2377658555a0fa7a40bed5e501db52167ce31758
obsidian <stratosphere.programming@gmail.com>
2010-06-14 10:03:52 -0500

    Update the ignore node object

--- Includes/Node/Ignore.php
+++ Includes/Node/Ignore.php
@@ -5,13 +5,13 @@
  *
  *  Failnet -- PHP-based IRC Bot
  *-------------------------------------------------------------------
- * @version		3.0.0 DEV
- * @category	Failnet
- * @package		nodes
- * @author		Failnet Project
- * @copyright	(c) 2009 - 2010 -- Failnet Project
- * @license		GNU General Public License, Version 3
- * @link		http://github.com/Obsidian1510/Failnet-PHP-IRC-Bot
+ * @version     3.0.0 DEV
+ * @category    Failnet
+ * @package     node
+ * @author      Failnet Project
+ * @copyright   (c) 2009 - 2010 -- Failnet Project
+ * @license     GNU General Public License, Version 3
+ * @link        http://github.com/Obsidian1510/Failnet-PHP-IRC-Bot
  *
  *===================================================================
  *
@@ -35,19 +35,21 @@
  *
  */
 
+namespace Failnet\Node;
+use Failnet;
 
 /**
  * Failnet - Ignore handling class,
  * 		Used as Failnet's handler for ignoring users based on hostmasks.
  *
  *
- * @category	Failnet
- * @package		nodes
- * @author		Failnet Project
- * @license		GNU General Public License, Version 3
- * @link		http://github.com/Obsidian1510/Failnet-PHP-IRC-Bot
+ * @category    Failnet
+ * @package     node
+ * @author      Failnet Project
+ * @license     GNU General Public License, Version 3
+ * @link        http://github.com/Obsidian1510/Failnet-PHP-IRC-Bot
  */
-class failnet_node_ignore extends failnet_common
+class Ignore extends Base
 {
 	/**
 	 * preg_match pattern cache used to check for an ignored user
@@ -62,20 +64,26 @@ class failnet_node_ignore extends failnet_common
 	private $users = array();
 
 	/**
-	 * Specialized init function to allow class construction to be easier.
-	 * @see includes/failnet_common#init()
+	 * Sets up the prepared SQL statements for the ignore package
 	 * @return void
 	 */
-	public function init()
+	public function __construct()
 	{
-		$this->failnet->ui->ui_system('--- Loading ignored users list...');
-		$this->sql('ignore', 'create', 'INSERT INTO ignore ( ignore_date, hostmask ) VALUES ( :timestamp, :hostmask )');
-		$this->sql('ignore', 'delete', 'DELETE FROM ignore WHERE LOWER(hostmask) = LOWER(:hostmask)');
-		$this->sql('ignore', 'get_single', 'SELECT * FROM ignore WHERE LOWER(hostmask) = LOWER(:hostmask) LIMIT 1');
-		$this->sql('ignore', 'get', 'SELECT * FROM ignore');
+		Bot::core('db')->armQuery('ignore', 'create', 'INSERT INTO ignore ( ignore_date, hostmask ) VALUES ( :timestamp, :hostmask )');
+		Bot::core('db')->armQuery('ignore', 'delete', 'DELETE FROM ignore WHERE LOWER(hostmask) = LOWER(:hostmask)');
+		Bot::core('db')->armQuery('ignore', 'get_single', 'SELECT * FROM ignore WHERE LOWER(hostmask) = LOWER(:hostmask) LIMIT 1');
+		Bot::core('db')->armQuery('ignore', 'get', 'SELECT * FROM ignore');
+	}
 
-		$this->failnet->sql('ignore', 'get')->execute();
-		$this->users = $this->failnet->sql('ignore', 'get')->fetchAll(PDO::FETCH_COLUMN, 0);
+	/**
+	 * Loads up the ignored users list
+	 * @return void
+	 */
+	public function loadIgnore()
+	{
+		Bot::core('ui')->system('--- Loading ignored users list...');
+		Bot::core('db')->useQuery('ignore', 'get')->execute();
+		$this->users = Bot::core('db')->useQuery('ignore', 'get')->fetchAll(PDO::FETCH_COLUMN, 0);
 		$this->cache = hostmasks_to_regex($this->users);
 	}
 
@@ -84,27 +92,24 @@ class failnet_node_ignore extends failnet_common
 	 * @param string $target - The hostmask to check
 	 * @return boolean - True if the hostmask is ignored, false if not.
 	 */
-	public function ignored($target)
+	public function isIgnored($target)
 	{
-		// Are _any_ hostmasks ignored?
-		if(empty($this->users))
-			return false;
-		return preg_match($this->cache, $target);
+		return ($this->users) ? preg_match($this->cache, $target) : false;
 	}
 
 	/**
 	 * Adds the specified target user to the ignored users list.
 	 * @param string $hostmask - The sender's hostmask
 	 * @param string $target - The target hostmask that should be added to the ignore list
-	 * @return True on success, false on hostmask already being ignored, NULL if not authed for this
+	 * @return boolean True on success, false on hostmask already being ignored
 	 */
-	public function add_ignore($hostmask, $target)
+	public function addIgnore($hostmask, $target)
 	{
 		// Check to see if this user would already be ignored...
-		if(!$this->ignored($target))
+		if(!$this->isIgnored($target))
 		{
 			// Do that SQL thang
-			$this->failnet->sql('ignore', 'create')->execute(array(':timestamp' => time(), ':hostmask' => $target));
+			Bot::core('db')->useQuery('ignore', 'create')->execute(array(':timestamp' => time(), ':hostmask' => $target));
 
 			// Now we need to rebuild the cached PCRE pattern
 			$this->users[] = $target;
@@ -121,15 +126,15 @@ class failnet_node_ignore extends failnet_common
 	 * Removes a specified hostmask pattern from the ignored users list.
 	 * @param string $hostmask - The sender's hostmask
 	 * @param string $target - The target hostmask to be removed from the ignore list
-	 * @return True on success, false on hostmask not within the ignore list, NULL if not authed for this
+	 * @return boolean True on success, false on hostmask not within the ignore list
 	 */
-	public function del_ignore($hostmask, $target)
+	public function delIgnore($hostmask, $target)
 	{
 		// Check to see if this hostmask IS in the ignored list
 		if(in_array($target, $this->users))
 		{
 			// Do that SQL thang
-			$this->failnet->sql('ignore', 'delete')->execute(array(':hostmask' => $target));
+			Bot::core('db')->useQuery('ignore', 'delete')->execute(array(':hostmask' => $target));
 
 			// Now we need to rebuild the cached PCRE pattern
 			foreach($this->users as $i => $user)
