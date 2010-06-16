4c3a710e251618ff50585afc6a393a46cea5416e
obsidian <stratosphere.programming@gmail.com>
2010-06-16 14:33:00 -0500

    update the cron core object with some new toys

--- Includes/Core/Cron.php
+++ Includes/Core/Cron.php
@@ -46,34 +46,85 @@ use Failnet;
  */
 class Cron extends Base
 {
-	public $last_event = 0;
+	//public $last_event = 0;
 
+	/**
+	 * @var array - The list of times when each task is scheduled to run next
+	 */
 	public $task_times = array();
 
-	public function __construct()
-	{
-		// meh
-	}
-
+	/**
+	 * Loads a task and prepares it for use
+	 * @param string $task_name - The name of the task to load
+	 * @return void
+	 * @throws Failnet\Exception
+	 */
 	public function addTask($task_name)
 	{
-
 		if(!Autoload::fileExists('Failnet\\Cron\\' . ucfirst($task_name)))
 			throw new Exception(ex(Exception::ERR_CRON_LOAD_FAILED, $task_name));
-		Bot::setCron(strtolower($task_name), 'Failnet\\Cron\\' . ucfirst($task_name));
+		Bot::setCron($task_name, 'Failnet\\Cron\\' . ucfirst($task_name));
+		$this->getTaskQueue($task_name);
 	}
 
+	/**
+	 * Changes the state of the specified task
+	 * @param string $task_name - The name of the task to change the state of
+	 * @param integer $status - The state to set the task to (must be a TASK_* constant)
+	 * @return boolean - Whether or not we were successful
+	 * @throws Failnet\Exception
+	 */
 	public function toggleTask($task_name, $status)
 	{
 		if(!in_array($status, array(TASK_ACTIVE, TASK_MANUAL, TASK_ZOMBIE)))
-			throw new Exception(); // @todo exception msg
+			throw new Exception(ex(Exception::ERR_CRON_INVALID_STATE));
 		if(!Bot::checkCronLoaded($task_name))
 			return false;
-		Bot::cron(strtolower($task_name))->status = $status;
+		Bot::cron($task_name)->status = $status;
 		return true;
 	}
 
-	public function runTasks() { }
+	/**
+	 * Automatically run all necessary tasks
+	 * @return void
+	 */
+	public function runTasks()
+	{
+		// run all tasks that can be run automatically
+		asort($this->task_times, SORT_NUMERIC);
+		foreach($this->task_times as $task => $time)
+		{
+			if($time > time())
+				continue;
+			Bot::cron($task)->autoRunTask();
+		}
 
-	public function triggerTask() { }
+	}
+
+	/**
+	 * Trigger a task manually
+	 * @param string $task_name - The name of the task we want to trigger.
+	 * @return mixed - Whatever the task returns.
+	 */
+	public function triggerTask($task_name)
+	{
+		return Bot::cron($task_name)->manualRunTask();
+	}
+
+	/**
+	 * Get the next time a task must be run, so long as it is setup to be automatically run
+	 * @param string $task_name - The name of the task to check
+	 * @return boolean - Whether or not the specified task can be automatically run.
+	 */
+	public function getTaskQueue($task_name)
+	{
+		// get the next time a task must be run
+		// If we're a zombie task or a manual task, we should not be queued into the task list.
+		if(Bot::cron($task_name)->status !== TASK_ACTIVE)
+			return false;
+
+		$next_run = (int) Bot::cron($task_name)->getNextRun();
+		$this->task_times[$task_name] = $next_run;
+		return true;
+	}
 }
