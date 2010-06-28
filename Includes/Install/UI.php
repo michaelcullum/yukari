87130c1f77d6fc2d0e0b6f80a4dbe0c360a6f3d4
obsidian <stratosphere.programming@gmail.com>
2010-06-28 15:41:46 -0500

    Fill out the Installer UI extension object with the methods we need. :O

--- Includes/Install/UI.php
+++ Includes/Install/UI.php
@@ -38,23 +38,81 @@ use Failnet\Lib as Lib;
  */
 class UI extends Core\UI
 {
-	public function stdinPrompt($name, $instruction, $prompt, $default)
+	/**
+	 * @var array - Array of valid strings that can be used as input for boolean true values.
+	 */
+	protected $bool_yes_vals = array('y' => true, '1' => true, 't' => true, 'yes' => true, 'enable' => true, 'true' => true, 'on' => true);
+
+	/**
+	 * @var array - Array of valid strings that can be used as input for boolean false values.
+	 */
+	protected $bool_no_vals = array('n' => false, '0' => false, 'f' => false, 'no' => false, 'disable' => false, 'false' => false, 'off' => false);
+
+	/**
+	 * Builds a prompt for information from STDIN, so we can ask the user for something.
+	 * @param string $instruction - The instruction text to provide the user so they know what we're asking.
+	 * @param mixed $default - The default value for the question, may be of any type.
+	 * @param string $prompt - The prompt text.
+	 * @return mixed - The user input directly from STDIN, with the ending PHP_EOL stripped.
+	 */
+	public function stdinPrompt($instruction, $default, $prompt)
 	{
-		// blah
+		if($instruction)
+			$this->output($instruction);
+
+		if($prompt)
+			echo $prompt . ' ';
+
+		$input = rtrim(fgets(STDIN), PHP_EOL);
+		return (!$input) ? $default : $input;
 	}
 
-	public function getBool($key, $default)
+	/**
+	 * Get a boolean value answer from the user.
+	 * @param string $instruction - The instruction text to provide the user so they know what we're asking.
+	 * @param boolean $default - The default value for the question.
+	 * @param string $prompt - The prompt text.
+	 * @return boolean - Desired user input.
+	 */
+	public function getBool($instruction, $default, $prompt = 'y/n')
 	{
-		// blah
+		$values = array_merge($this->bool_yes_vals, $this->bool_no_vals);
+
+		// Nag the user for a usable answer
+		$validates = false;
+		do
+		{
+			$input = strtolower($this->stdinPrompt($instruction, (boolean) $default, $prompt));
+			$validates = isset($values[$input]);
+			if(!$validates)
+				$this->output('Invalid response', 'ERROR');
+		}
+		while(!$validates);
+
+		return (boolean) $values[$input];
 	}
 
-	public function getString($key, $default)
+	/**
+	 * Get a string value answer from the user.
+	 * @param string $instruction - The instruction text to provide the user so they know what we're asking.
+	 * @param string $default - The default value for the question.
+	 * @param string $prompt - The prompt text.
+	 * @return string - Desired user input.
+	 */
+	public function getString($instruction, $default, $prompt)
 	{
-		// blah
+		return (string) $this->stdinPrompt($instruction, (string) $default, $prompt);
 	}
 
-	public function getInt($key, $default)
+	/**
+	 * Get a integer value answer from the user.
+	 * @param string $instruction - The instruction text to provide the user so they know what we're asking.
+	 * @param integer $default - The default value for the question.
+	 * @param string $prompt - The prompt text.
+	 * @return integer - Desired user input.
+	 */
+	public function getInt($instruction, $default, $prompt)
 	{
-		// blah
+		return (int) $this->stdinPrompt($instruction, (int) $default, $prompt);
 	}
 }
