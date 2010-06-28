0cfb4394882537bd4b45f2a8bff77eee437d3270
obsidian <stratosphere.programming@gmail.com>
2010-06-28 12:51:28 -0500

    commit an example language file

---
+++ Data/Language/Core.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ *
+ *===================================================================
+ *
+ *  Failnet -- PHP-based IRC Bot
+ *-------------------------------------------------------------------
+ * @version     3.0.0 DEV
+ * @category    Failnet
+ * @package     language
+ * @author      Damian Bushong
+ * @copyright   (c) 2009 - 2010 -- Damian Bushong
+ * @license     MIT License
+ * @link        http://github.com/Obsidian1510/Failnet-PHP-IRC-Bot
+ *
+ *===================================================================
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this package in the file LICENSE.
+ *
+ */
+
+namespace Failnet\Lang;
+use Failnet;
+
+/**
+ * @ignore
+ */
+Bot::core('lang')->setEntries(array(
+	'LANGUAGE_VAR'	=> 'Some language variable: %1$s'
+));
