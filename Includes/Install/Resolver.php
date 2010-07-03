6617fdd3dd08efa91cab71a86a1094141fd56ed2
obsidian <stratosphere.programming@gmail.com>
2010-07-02 21:26:45 -0500

    very, very rough beginning of the dependency resolution code

---
+++ Includes/Install/Resolver.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ *
+ *===================================================================
+ *
+ *  Failnet -- PHP-based IRC Bot
+ *-------------------------------------------------------------------
+ * @version     3.0.0 DEV
+ * @category    Failnet
+ * @package     install
+ * @author      Damian Bushong
+ * @copyright   (c) 2009 - 2010 -- Damian Bushong
+ * @license     MIT License
+ * @link        http://github.com/Obsidian1510/Failnet-PHP-IRC-Bot
+ *
+ *===================================================================
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this package in the file LICENSE.
+ *
+ */
+
+namespace Failnet\Install;
+use Failnet;
+use Failnet\Core as Core;
+use Failnet\Lib as Lib;
+
+/**
+ * Failnet - Dependency processing and resolution class,
+ *      Processes dependencies to detect broken or unsupported packages.
+ *
+ *
+ * @category    Failnet
+ * @package     install
+ * @author      Damian Bushong
+ * @license     MIT License
+ * @link        http://github.com/Obsidian1510/Failnet-PHP-IRC-Bot
+ */
+class Resolver extends Base
+{
+	public $dependencies = array();
+
+	public function loadFile($namespace, $version, array $dependencies = array())
+	{
+		// meh
+	}
+
+	public function resolveFile($namespace)
+	{
+		// blah
+	}
+
+	public function checkBroken($namespace)
+	{
+		// snork
+	}
+}
