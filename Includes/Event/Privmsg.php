b44e8447c7136b76d769461fee843e41fc59e9c1
Obsidian <stratosphere.programming@gmail.com>
2010-08-20 16:26:48 -0500

    Toss in the privmsg event type

---
+++ Includes/Event/Privmsg.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ *
+ *===================================================================
+ *
+ *  Failnet -- PHP-based IRC Bot
+ *-------------------------------------------------------------------
+ * @version     3.0.0 DEV
+ * @category    Failnet
+ * @package     event
+ * @author      Damian Bushong
+ * @copyright   (c) 2009 - 2010 -- Damian Bushong
+ * @license     MIT License
+ * @link        http://github.com/Obsidian1510/Failnet-PHP-IRC-Bot
+ *
+ *===================================================================
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this package in the file LICENSE.
+ *
+ */
+
+namespace Failnet\Event;
+
+/**
+ * Failnet - Event object,
+ * 	    A class to define ene of the various event types.
+ *
+ *
+ * @category    Failnet
+ * @package     event
+ * @author      Damian Bushong
+ * @license     MIT License
+ * @link        http://github.com/Obsidian1510/Failnet-PHP-IRC-Bot
+ */
+class Privmsg extends Failnet\Event\EventBase
+{
+	/**
+	 * @var array - Array mapping args for quick setting later
+	 */
+	protected $map = array(
+		'target',
+		'text'
+	);
+
+	/**
+	 * @var string - Target arg for the PRIVMSG event type.
+	 */
+	public $arg_target = '';
+
+	/**
+	 * @var string - Text arg for the PRIVMSG event type.
+	 */
+	public $arg_text = '';
+
+	/**
+	 * Grab the event's buffer, useful when sending events
+	 * @return string - The event buffer.
+	 */
+	public function getBuffer()
+	{
+		return 'PRIVMSG ' . $this->arg_target . ' :' . $this->arg_text;
+	}
+}
