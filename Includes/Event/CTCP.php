0fba59039d0e719775d50618470b61594a6c45d7
obsidian <stratosphere.programming@gmail.com>
2010-09-14 16:03:14 -0500

    let there be a ctcp event object!
    and there was.

---
+++ Includes/Event/CTCP.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ *
+ *===================================================================
+ *
+ *  Failnet -- PHP-based IRC Bot
+ *-------------------------------------------------------------------
+ * @version     3.0.0 DEV
+ * @category    Failnet
+ * @package     event
+ * @author      Damian Bushong
+ * @copyright   (c) 2009 - 2010 -- Damian Bushong
+ * @license     MIT License
+ * @link        http://github.com/Obsidian1510/Failnet-PHP-IRC-Bot
+ *
+ *===================================================================
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this package in the file LICENSE.
+ *
+ */
+
+namespace Failnet\Event;
+
+/**
+ * Failnet - Event object,
+ * 	    A class to define ene of the various event types.
+ *
+ *
+ * @category    Failnet
+ * @package     event
+ * @author      Damian Bushong
+ * @license     MIT License
+ * @link        http://github.com/Obsidian1510/Failnet-PHP-IRC-Bot
+ */
+class CTCP extends Failnet\Event\EventBase
+{
+	/**
+	 * @var array - Array mapping args for quick setting later
+	 */
+	protected $map = array(
+		'target',
+		'command',
+		'args',
+	);
+
+	/**
+	 * @var string - Event arg.
+	 */
+	public $arg_target = '';
+
+	/**
+	 * @var string - Event arg.
+	 */
+	public $arg_command = '';
+
+	/**
+	 * @var array - Event arg.
+	 */
+	public $arg_args = array();
+
+	/**
+	 * Build the IRC command from the args included
+	 * @return string - The raw command to send.
+	 */
+	public function buildCommand()
+	{
+		if(!empty($this['args']))
+		{
+			return sprintf('NOTICE %1$s :' . chr(1) . '%2$s %3$s' . chr(1), $this['target'], strtoupper($this['command']), implode(' ', $this['args']));
+		}
+		else
+		{
+			return sprintf('NOTICE %1$s :' . chr(1) . '%2$s' . chr(1), $this['target'], strtoupper($this['command']));
+		}
+	}
+}
