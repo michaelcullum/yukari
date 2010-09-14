1f58b1fa23e7b77b4d87ea260b9e0d62715a12b5
obsidian <stratosphere.programming@gmail.com>
2010-09-14 16:25:09 -0500

    here's the start of the session object stuff...wooo

---
+++ Includes/User/Session.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ *
+ *===================================================================
+ *
+ *  Failnet -- PHP-based IRC Bot
+ *-------------------------------------------------------------------
+ * @version     3.0.0 DEV
+ * @category    Failnet
+ * @package     Failnet
+ * @author      Damian Bushong
+ * @copyright   (c) 2009 - 2010 -- Damian Bushong
+ * @license     MIT License
+ * @link        http://github.com/Obsidian1510/Failnet-PHP-IRC-Bot
+ *
+ *===================================================================
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this package in the file LICENSE.
+ *
+ */
+
+namespace Failnet\User;
+use Failnet as Root;
+use Failnet\Bot as Bot;
+use Failnet\Lib as Lib;
+
+/**
+ * Failnet - Session interface,
+ *      Prototype that defines methods that session objects must implement.
+ *
+ *
+ * @category    Failnet
+ * @package     core
+ * @author      Damian Bushong
+ * @license     MIT License
+ * @link        http://github.com/Obsidian1510/Failnet-PHP-IRC-Bot
+ */
+class Session extends Root\Hookable implements SessionInterface
+{
+	public function __construct(Failnet\Lib\Hostmask $hostmask)
+	{
+		// asdf
+	}
+
+	public function login($password)
+	{
+		// asdf
+	}
+
+	public function logout()
+	{
+		// asdf
+	}
+
+	public function getLastActive()
+	{
+		// asdf
+	}
+
+	public function setLastActive($time)
+	{
+		// asdf
+	}
+
+	public function onDestroy()
+	{
+		// asdf
+	}
+}
---
+++ Includes/User/SessionInterface.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ *
+ *===================================================================
+ *
+ *  Failnet -- PHP-based IRC Bot
+ *-------------------------------------------------------------------
+ * @version     3.0.0 DEV
+ * @category    Failnet
+ * @package     Failnet
+ * @author      Damian Bushong
+ * @copyright   (c) 2009 - 2010 -- Damian Bushong
+ * @license     MIT License
+ * @link        http://github.com/Obsidian1510/Failnet-PHP-IRC-Bot
+ *
+ *===================================================================
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this package in the file LICENSE.
+ *
+ */
+
+namespace Failnet\User;
+use Failnet as Root;
+use Failnet\Bot as Bot;
+use Failnet\Lib as Lib;
+
+/**
+ * Failnet - Session interface,
+ *      Prototype that defines methods that session objects must implement.
+ *
+ *
+ * @category    Failnet
+ * @package     core
+ * @author      Damian Bushong
+ * @license     MIT License
+ * @link        http://github.com/Obsidian1510/Failnet-PHP-IRC-Bot
+ */
+interface SessionInterface extends \ArrayAccess
+{
+	public function __construct(Failnet\Lib\Hostmask $hostmask);
+	public function login($password);
+	public function logout();
+	public function getLastActive();
+	public function setLastActive($time);
+	public function onDestroy();
+}
