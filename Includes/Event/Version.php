e672ad94d0b79c95f89fda7d07b41cd7e26dd943
obsidian <stratosphere.programming@gmail.com>
2010-09-14 16:08:49 -0500

    to top it off, now there's a version event object too!  GEE WILLIKERS BATMAN!

---
+++ Includes/Event/Version.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ *
+ *===================================================================
+ *
+ *  Failnet -- PHP-based IRC Bot
+ *-------------------------------------------------------------------
+ * @version     3.0.0 DEV
+ * @category    Failnet
+ * @package     event
+ * @author      Damian Bushong
+ * @copyright   (c) 2009 - 2010 -- Damian Bushong
+ * @license     MIT License
+ * @link        http://github.com/Obsidian1510/Failnet-PHP-IRC-Bot
+ *
+ *===================================================================
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this package in the file LICENSE.
+ *
+ */
+
+namespace Failnet\Event;
+
+/**
+ * Failnet - Event object,
+ * 	    A class to define ene of the various event types.
+ *
+ *
+ * @category    Failnet
+ * @package     event
+ * @author      Damian Bushong
+ * @license     MIT License
+ * @link        http://github.com/Obsidian1510/Failnet-PHP-IRC-Bot
+ */
+class Version extends Failnet\Event\CTCP
+{
+	/**
+	 * @var string - Event arg.
+	 */
+	public $arg_version = '';
+
+	/**
+	 * Build the IRC command from the args included
+	 * @return string - The raw command to send.
+	 */
+	public function buildCommand()
+	{
+		$this['command'] = 'VERSION';
+
+		if(!empty($this['version']))
+			$this['args'][] = $this['version'];
+
+		parent::buildCommand();
+	}
+}
