152eafee878d8d0e5d2619b54fea5d36a344d86b
obsidian <stratosphere.programming@gmail.com>
2010-09-14 16:19:57 -0500

    how about that join event object there

---
+++ Includes/Event/Join.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ *
+ *===================================================================
+ *
+ *  Failnet -- PHP-based IRC Bot
+ *-------------------------------------------------------------------
+ * @version     3.0.0 DEV
+ * @category    Failnet
+ * @package     event
+ * @author      Damian Bushong
+ * @copyright   (c) 2009 - 2010 -- Damian Bushong
+ * @license     MIT License
+ * @link        http://github.com/Obsidian1510/Failnet-PHP-IRC-Bot
+ *
+ *===================================================================
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this package in the file LICENSE.
+ *
+ */
+
+namespace Failnet\Event;
+
+/**
+ * Failnet - Event object,
+ * 	    A class to define ene of the various event types.
+ *
+ *
+ * @category    Failnet
+ * @package     event
+ * @author      Damian Bushong
+ * @license     MIT License
+ * @link        http://github.com/Obsidian1510/Failnet-PHP-IRC-Bot
+ */
+class Join extends Failnet\Event\EventBase
+{
+	/**
+	 * @var array - Array mapping args for quick setting later
+	 */
+	protected $map = array(
+		'channel',
+		'key',
+	);
+
+	/**
+	 * @var string - Event arg.
+	 */
+	public $arg_channel = '';
+
+	/**
+	 * @var string - Event arg.
+	 */
+	public $arg_key = '';
+
+	/**
+	 * Build the IRC command from the args included
+	 * @return string - The raw command to send.
+	 */
+	public function buildCommand()
+	{
+		if(!empty($this['key']))
+		{
+			return sprintf('JOIN %1$s :%2$s', $this['channel'], $this['key']);
+		}
+		else
+		{
+			return sprintf('JOIN %1$s', $this['channel']);
+		}
+	}
+}
