dbf31a027465c50b1d1ca8595a0b3dcb3e7e52de
obsidian <stratosphere.programming@gmail.com>
2010-09-21 16:53:23 -0500

    lets add docs and the new flash storage (plus arrayaccess methods) to the session base code

--- Includes/Core/Session/SessionBase.php
+++ Includes/Core/Session/SessionBase.php
@@ -7,7 +7,7 @@
  *-------------------------------------------------------------------
  * @version     3.0.0 DEV
  * @category    Failnet
- * @package     Failnet
+ * @package     core
  * @author      Damian Bushong
  * @copyright   (c) 2009 - 2010 -- Damian Bushong
  * @license     MIT License
@@ -25,19 +25,136 @@ use Failnet as Root;
 use Failnet\Bot as Bot;
 use Failnet\Lib as Lib;
 
+/**
+ * Failnet - Session base class,
+ *      Defines common methods and properties for session objects to use.
+ *
+ *
+ * @category    Failnet
+ * @package     core
+ * @author      Damian Bushong
+ * @license     MIT License
+ * @link        http://github.com/Obsidian1510/Failnet3
+ */
 class SessionBase implements SessionInterface
 {
+	/**
+	 * @var Failnet\Core\ACL\ACLBase - The ACL object that accompanies this session
+	 */
 	public $acl;
 
+	/**
+	 * @var Failnet\Lib\Hostmask - The hostmask that this session object represents
+	 */
 	public $hostmask;
 
+	/**
+	 * @var string - The pointer string that is used to refer to this session with
+	 */
 	public $pointer = '';
 
-	public $session_key = '';
+	/**
+	 * @var string - The session ID that is assigned to this session
+	 */
+	public $session_id = '';
 
-	public function __construct(Lib\Hostmask $hostmask, $session_key, $pointer)
+	/**
+	 * @var integer - The timestamp that the session was last active
+	 */
+	public $last_active = 0;
+
+	/**
+	 * @var array - Array of various snips of data related to this session to store while the session is active
+	 */
+	public $data = array();
+
+	/**
+	 * @var array - Array of bits of data that can be retrieved once and only once
+	 */
+	private $flash = array();
+
+	/**
+	 * Constructor
+	 * @param Failnet\Lib\Hostmask $hostmask - The hostmask that this session object represents
+	 * @param string $session_key - The session key assigned to this session
+	 * @param string $pointer - The pointer that references this session in the core auth object
+	 * @return void
+	 */
+	public function __construct(Lib\Hostmask $hostmask, $session_id, $pointer)
 	{
-		list($this->hostmask, $this->session_key, $this->pointer) = array($hostmask, $session_key, $pointer);
+		list($this->hostmask, $this->session_id, $this->pointer) = array($hostmask, $session_id, $pointer);
+	}
+
+	/**
+	 * Obtain or set a "flash" data index, or a value that can be retrieved once and only once
+	 * @param string $flash_key - The key to use for the "flashed" data
+	 * @param mixed $flash_value - The data to "flash" onto the session. If null, this method will attempt to return stored "flash" data if present instead, and then destroy it.
+	 * @return mixed - If $flash_value is NULL, return is mixed (the "flashed" data that was stored or NULL if nothing present), or it will return void.
+	 *
+	 * @note: this method is declared final for a reason!
+	 */
+	final public function flash($flash_key, $flash_value = NULL)
+	{
+		if(is_null($flash_value))
+		{
+			if(isset($this->flash[$flash_key]))
+			{
+				$flash = $this->flash[$flash_key];
+				// we've grabbed the data, and now we have to wipe it from the session object
+				unset($this->flash[$flash_key]);
+				return $flash;
+			}
+			return NULL;
+		}
+		else
+		{
+			$this->flash[$flash_key] = $flash_value;
+		}
+	}
+
+	/**
+	 * ArrayAccess methods
+	 */
+
+	/**
+	 * Check if an "array" offset exists in this object.
+	 * @param mixed $offset - The offset to check.
+	 * @return boolean - Does anything exist for this offset?
+	 */
+	public function offsetExists($offset)
+	{
+		return isset($this->data[$offset]);
+	}
+
+	/**
+	 * Get an "array" offset for this object.
+	 * @param mixed $offset - The offset to grab from.
+	 * @return mixed - The value of the offset, or null if the offset does not exist.
+	 */
+	public function offsetGet($offset)
+	{
+		return isset($this->data[$offset]) ? $this->data[$offset] : NULL;
+	}
+
+	/**
+	 * Set an "array" offset to a certain value, if the offset exists
+	 * @param mixed $offset - The offset to set.
+	 * @param mixed $value - The value to set to the offset.
+	 * @return void
+	 */
+	public function offsetSet($offset, $value)
+	{
+		$this->data[$offset] = $value;
+	}
+
+	/**
+	 * Unset an "array" offset.
+	 * @param mixed $offset - The offset to clear out.
+	 * @return void
+	 */
+	public function offsetUnset($offset)
+	{
+		unset($this->data[$offset]);
 	}
 }
 
@@ -54,7 +171,8 @@ class SessionBase implements SessionInterface
  */
 interface SessionInterface extends \ArrayAccess
 {
-	public function __construct(Lib\Hostmask $hostmask, $session_key, $pointer);
+	public function __construct(Lib\Hostmask $hostmask, $session_id, $pointer);
+	public function flash($flash_key, $flash_value = NULL);
 	public function login($password);
 	public function logout();
 	public function getLastActive();
