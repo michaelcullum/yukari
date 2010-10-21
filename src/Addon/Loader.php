8103e104ebc1f1f6e80f35bb6591d85ceda46a28
Obsidian <stratosphere.programming@gmail.com>
2010-10-21 12:23:50 -0500

    iterator methods in Failnet\Addon\Loader are now in place

--- src/Addon/Loader.php
+++ src/Addon/Loader.php
@@ -35,7 +35,7 @@ use Failnet\Lib as Lib;
  * @license     MIT License
  * @link        http://github.com/Obsidian1510/Failnet3
  */
-class Loader implements Iterator // @todo iterator methods
+class Loader implements Iterator
 {
 	/**
 	 * @var array - Array of instantiated metadata objects.
@@ -77,7 +77,12 @@ class Loader implements Iterator // @todo iterator methods
 	 */
 	public function loadAddon($addon)
 	{
+		// Check to see if the addon has already been loaded.
+		if(isset($this->metadata[hash('md5', $addon)]))
+			return;
+
 		$using_phar = false;
+		// Check to see if there's a phar we are dealing with here before moving on to try to load the standard class files.
 		$phar_path = FAILNET . 'addons/' . $addon . '/' . $addon . '.phar';
 		if(file_exists($phar_path))
 		{
@@ -113,6 +118,7 @@ class Loader implements Iterator // @todo iterator methods
 		if(!$metadata->checkDependencies())
 			throw new LoaderException('Addon metadata object declares that its required dependencies have not been met', LoaderException::ERR_METADATA_CUSTOM_DEPENDENCY_FAIL);
 
+		// If we aren't using a phar here we need to add the addon's path to the autoload paths.
 		if(!$using_phar)
 		{
 			// If the addon's metadata object passes all checks and we're not using a phar file, then we add the addon's directory to the autoloader include path
@@ -123,6 +129,55 @@ class Loader implements Iterator // @todo iterator methods
 		$metadata->initialize();
 
 		// Store the metadata object in a predictable slot.
-		$this->metadata[hash('md5', $metadata->getName())] = $metadata;
+		$this->metadata[hash('md5', $addon)] = $metadata;
+	}
+
+	/**
+	 * Iterator methods
+	 */
+
+	/**
+	 * Iterator method, rewinds the array back to the first element.
+	 * @return void
+	 */
+	public function rewind()
+	{
+		return reset($this->metadata);
+	}
+
+	/**
+	 * Iterator method, returns the key of the current element
+	 * @return scalar - The key of the current element.
+	 */
+	public function key()
+	{
+		return key($this->metadata);
+	}
+
+	/**
+	 * Iterator method, checks to see if the current position is valid.
+	 * @return boolean - Whether or not the current array position is valid.
+	 */
+	public function valid()
+	{
+		return (!is_null(key($this->metadata)));
+	}
+
+	/**
+	 * Iterator method, gets the current element
+	 * @return Failnet\Addon\Metadata\MetadataBase - The current addon metadata object of focus.
+	 */
+	public function current()
+	{
+		return current($this->metadata);
+	}
+
+	/**
+	 * Iterator method, moves to the next session available.
+	 * @return void
+	 */
+	public function next()
+	{
+		next($this->metadata);
 	}
 }
