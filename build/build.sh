1663b34dff76024b5db5c7db7d25ec35060a3b69
Obsidian <stratosphere.programming@gmail.com>
2011-01-23 09:08:46 -0600

    overhaul the build scripts

--- build/build.sh
+++ build/build.sh
@@ -1,22 +1,49 @@
 #! /bin/bash
 
+#
+#===================================================================
+#  Yukari
+#-------------------------------------------------------------------
+# Copyright:    (c) 2009 - 2011 -- Damian Bushong
+# License:      MIT License
+#
+#===================================================================
+#
+# This source file is subject to the MIT license that is bundled
+# with this package in the file LICENSE.
+#
+
+##########################################
+# begin script config
+##########################################
+
+# files to exclude in phar-build
+EXCLUDE="~$ .*\.txt$ .*\.markdown$ .*\.md$ stub\.php .*\.json$"
+# directories to exclude in phar-build
+EXLUDEDIR="/Language/Package/*"
+# source directory
+SRC="./src/"
+# name of the phar archive
+PHARNAME=yukari.phar
+
+##########################################
+# end script config
+##########################################
+
+# get this script's full path
 SCRIPT=`dirname $(readlink -f $0)`
-$SCRIPT/build-check --src ./src/ --exclude "~$ .*\.txt$ .*\.markdown$ .*\.md$ .*\.json$" --exclude-dir "/Language/Package/*"
-RESULT=`cat $SCRIPT/rebuild`
-if [ $RESULT = '1' ]; then
+#cd $SCRIPT/../
+phar-file-checksums --src $SRC --exclude $EXCLUDE --exclude-dir $EXCLUDEDIR --checksumfile ./filestate
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+	echo 'no rebuild needed'
+else
 	echo 'updating phar file'
-	phar-build --phar $SCRIPT/failnet.phar --src ./src/ --exclude "~$ .*\.txt$ .*\.markdown$ .*\.md$ .*\.json$" --exclude-dir "/Language/Package/*"
-	$SCRIPT/build-filestate-update --src ./src/ --exclude "~$ .*\.txt$ .*\.markdown$ .*\.md$ .*\.json$" --exclude-dir "/Language/Package/*"
-	cp $SCRIPT/failnet.phar $SCRIPT/../failnet.phar
-	cp $SCRIPT/failnet.phar.pubkey $SCRIPT/../failnet.phar.pubkey
-    rm $SCRIPT/failnet.phar
-    rm $SCRIPT/failnet.phar.pubkey
-	rm $SCRIPT/rebuild
+	phar-build --phar $SCRIPT/$PHARNAME --src $SRC --exclude $EXCLUDE --exclude-dir $EXCLUDEDIR --stub ./../src/stub.php
+	mv $SCRIPT/$PHARNAME $SCRIPT/../lib/$PHARNAME
+	mv $SCRIPT/$PHARNAME.pubkey $SCRIPT/../lib/$PHARNAME.pubkey
 	if [ -d $SCRIPT/../.git/ ]; then
-		git add $SCRIPT/../failnet.phar $SCRIPT/filestate.json
+		git add $SCRIPT/../lib/$PHARNAME $SCRIPT/filestate
 	fi
 	echo 'success'
-else
-	rm $SCRIPT/rebuild
-	echo 'no rebuild needed'
 fi
