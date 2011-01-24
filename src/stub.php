27346bb4caba56c71bd09dbb53f5c7b3ca1666b5
Obsidian <stratosphere.programming@gmail.com>
2011-01-23 21:40:05 -0600

    THE STUB FILE WOOO

---
+++ src/stub.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ *
+ *===================================================================
+ *
+ *  Yukari
+ *-------------------------------------------------------------------
+ * @category    Yukari
+ * @package     Yukari
+ * @author      Damian Bushong
+ * @copyright   (c) 2009 - 2011 -- Damian Bushong
+ * @license     MIT License
+ * @link        https://github.com/damianb/yukari
+ *
+ *===================================================================
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this package in the file LICENSE.
+ *
+ */
+
+// Gotta love one-liners.
+require 'phar://' . YUKARI_PHAR . '/Bootstrap.php';
+
+__HALT_COMPILER(); ?>
