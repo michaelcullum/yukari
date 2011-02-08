c876d717e24f2a29d48e64e59f7272603212f628
Obsidian <stratosphere.programming@gmail.com>
2011-02-08 13:05:02 -0600

    Another addon!

---
+++ addons/automation/Addon/Metadata/Automation.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ *
+ *===================================================================
+ *
+ *  Yukari
+ *-------------------------------------------------------------------
+ * @category    Yukari
+ * @package     addon
+ * @author      Damian Bushong
+ * @copyright   (c) 2009 - 2011 -- Damian Bushong
+ * @license     MIT License
+ * @link        https://github.com/damianb/yukari
+ *
+ *===================================================================
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this package in the file LICENSE.
+ *
+ */
+
+namespace Yukari\Addon\Metadata;
+use Yukari\Kernel;
+
+/**
+ * Yukari - Addon metadata object,
+ *      Provides some information regarding the addon.
+ *
+ *
+ * @category    Yukari
+ * @package     addon
+ * @author      Damian Bushong
+ * @license     MIT License
+ * @link        https://github.com/damianb/yukari
+ */
+class Automation extends \Yukari\Addon\Metadata\MetadataBase
+{
+	/**
+	 * @var string - The addon's version.
+	 */
+	protected $version = 'core';
+
+	/**
+	 * @var string - The addon's author information.
+	 */
+	protected $author = 'Damian Bushong';
+
+	/**
+	 * @var string - The addon's name.
+	 */
+	protected $name = 'Automation';
+
+	/**
+	 * @var string - The addon's description.
+	 */
+	protected $description = 'Provides basic automation for handling of standard IRC events.';
+
+	/**
+	 * Hooking method for addon metadata objects, called to initialize the addon after the dependency check has been passed.
+	 * @return void
+	 */
+	public function initialize()
+	{
+		$dispatcher = Kernel::getDispatcher();
+
+		$dispatcher->register('irc.input.ctcp', function(\Yukari\Event\Instance $event) {
+			if(strtolower($event['command']) === 'version')
+			{
+				return \Yukari\Event\Instance::newEvent(null, 'irc.output.ctcp_reply')
+					->setDataPoint('target', $event['target'])
+					->setDataPoint('command', 'version')
+					->setDataPoint('args', sprintf('Yukari IRC Bot - %s', Kernel::getBuildNumber()));
+			}
+			elseif(strtolower($event['command']) === 'ping')
+			{
+				return \Yukari\Event\Instance::newEvent(null, 'irc.output.ctcp_reply')
+					->setDataPoint('target', $event['target'])
+					->setDataPoint('command', 'ping')
+					->setDataPoint('args', $event['args']);
+			}
+			else
+			{
+				return NULL;
+			}
+		});
+
+		$dispatcher->register('irc.input.ping', function(\Yukari\Event\Instance $event) {
+			return \Yukari\Event\Instance::newEvent(null, 'irc.output.pong')
+					->setDataPoint('origin', $event['target']);
+		});
+	}
+
+	/**
+	 * Hooking method for addon metadata objects for executing own code on pre-load dependency check.
+	 * @return boolean - Does the addon pass the dependency check?
+	 */
+	public function checkDependencies()
+	{
+		// This addon has no dependencies.
+		return true;
+	}
+}
