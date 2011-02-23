5b6a3e1bdd2fe84fb7a6da9c78be62c8f3fb78b1
Obsidian <stratosphere.programming@gmail.com>
2011-02-22 18:14:24 -0600

    Add in the very shell of the usertracker addon

---
+++ addons/usertracker/Addon/DataTracking/UserTracker.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ *
+ *===================================================================
+ *
+ *  Yukari
+ *-------------------------------------------------------------------
+ * @category    Yukari
+ * @package     addon
+ * @author      Damian Bushong
+ * @copyright   (c) 2009 - 2011 -- Damian Bushong
+ * @license     MIT License
+ * @link        https://github.com/damianb/yukari
+ *
+ *===================================================================
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this package in the file LICENSE.
+ *
+ */
+
+namespace Yukari\Addon\DataTracker;
+use Yukari\Kernel;
+
+/**
+ * Yukari - User tracking object,
+ *      Populates and maintains a local cache of user hostmasks, and each user's channel modes in the channels that the bot is inhabiting.
+ *
+ *
+ * @category    Yukari
+ * @package     addon
+ * @author      Damian Bushong
+ * @license     MIT License
+ * @link        https://github.com/damianb/yukari
+ */
+class UserTracker
+{
+	// asdf
+}
---
+++ addons/usertracker/Addon/Metadata/Usertracker.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ *
+ *===================================================================
+ *
+ *  Yukari
+ *-------------------------------------------------------------------
+ * @category    Yukari
+ * @package     addon
+ * @author      Damian Bushong
+ * @copyright   (c) 2009 - 2011 -- Damian Bushong
+ * @license     MIT License
+ * @link        https://github.com/damianb/yukari
+ *
+ *===================================================================
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this package in the file LICENSE.
+ *
+ */
+
+namespace Yukari\Addon\Metadata;
+use Yukari\Kernel;
+
+/**
+ * Yukari - Addon metadata object,
+ *      Provides some information regarding the addon.
+ *
+ *
+ * @category    Yukari
+ * @package     addon
+ * @author      Damian Bushong
+ * @license     MIT License
+ * @link        https://github.com/damianb/yukari
+ */
+class Usertracker extends \Yukari\Addon\Metadata\MetadataBase
+{
+	/**
+	 * @var string - The addon's version.
+	 */
+	protected $version = 'core';
+
+	/**
+	 * @var string - The addon's author information.
+	 */
+	protected $author = 'Damian Bushong';
+
+	/**
+	 * @var string - The addon's name.
+	 */
+	protected $name = 'UserTracker';
+
+	/**
+	 * @var string - The addon's description.
+	 */
+	protected $description = 'Populates and maintains a local cache of user hostmasks, and each user\'s channel modes in the channels that the bot is inhabiting.';
+
+	/**
+	 * Hooking method for addon metadata objects, called to initialize the addon after the dependency check has been passed.
+	 * @return void
+	 */
+	public function initialize()
+	{
+		$usertracker = Kernel::set('addon.usertracker', new \Yukari\Addon\DataTracking\UserTracker());
+		$usertracker->registerListeners();
+	}
+
+	/**
+	 * Hooking method for addon metadata objects for executing own code on pre-load dependency check.
+	 * @return boolean - Does the addon pass the dependency check?
+	 *
+	 * @throws \RuntimeException
+	 */
+	public function checkDependencies()
+	{
+		// Commander addon necessary for this addon.
+		if(!Kernel::get('addon.commander'))
+			throw new \RuntimeException('Addon dependency "addon.commander" not present');
+		if(!Kernel::get('addon.channeltracker'))
+			throw new \RuntimeException('Addon dependency "addon.channeltracker" not present');
+
+		return true;
+	}
+}
