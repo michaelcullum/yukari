54df83d2eb924831c615ab68061184957812b964
Obsidian <stratosphere.programming@gmail.com>
2011-03-03 12:31:25 -0600

    Change these over for the new submodule (won't be needed in a bit I think, though)

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -51,10 +51,12 @@ require \Yukari\ROOT_PATH . '/Environment.php';
 if(!ini_get('date.timezone'))
 	@date_default_timezone_set(@date_default_timezone_get());
 
-// Include the sfYaml stuff (ugh include_once)
-include_once \Yukari\ROOT_PATH . '/vendor/sfYaml/lib/sfYaml.php';
-include_once \Yukari\ROOT_PATH . '/vendor/sfYaml/lib/sfYamlParser.php';
-include_once \Yukari\ROOT_PATH . '/vendor/sfYaml/lib/sfYamlInline.php';
+// Include the Symfony Yaml component
+require \Yukari\ROOT_PATH . '/vendor/Symfony/Yaml/Yaml.php';
+require \Yukari\ROOT_PATH . '/vendor/Symfony/Yaml/Exception.php';
+require \Yukari\ROOT_PATH . '/vendor/Symfony/Yaml/Parser.php';
+require \Yukari\ROOT_PATH . '/vendor/Symfony/Yaml/ParserException.php';
+require \Yukari\ROOT_PATH . '/vendor/Symfony/Yaml/Inline.php';
 
 // Run indefinitely...
 set_time_limit(0);
