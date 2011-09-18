70c6f32022ffc483bcd9ae54ecc2965acbac7811
damianb <damian@emberlabs.org>
2011-09-18 03:26:34 -0500

    Rework this a bit

--- src/Codebite/Yukari/Environment/Display.php
+++ src/Codebite/Yukari/Environment/Display.php
@@ -215,142 +215,6 @@ class Display
 		$dispatcher->register('ui.message.debug', array($this, 'displayDebug'));
 		$dispatcher->register('ui.message.raw', array($this, 'displayRaw'));
 
-		$dispatcher->register('ui.message.irc', array($this, 'displayIRC'));
-
-		// Display IRC going-ons
-		$dispatcher->register('irc.input.action', function(Event $event) use($dispatcher) {
-			$dispatcher->trigger(Event::newEvent('ui.message.irc')
-				->set('message', sprintf('<- [%2$s] *** %1$s %3$s', $event->get('hostmask')->getNick(), $event->get('target'), $event->get('text'))));
-		});
-		$dispatcher->register('irc.input.privmsg', function(Event $event) use($dispatcher) {
-			$dispatcher->trigger(Event::newEvent('ui.message.irc')
-				->set('message', sprintf('<- [%2$s] <%1$s> %3$s', $event->get('hostmask')->getNick(), $event->get('target'), $event->get('text'))));
-		});
-		$dispatcher->register('irc.input.notice', function(Event $event) use($dispatcher) {
-			$dispatcher->trigger(Event::newEvent('ui.message.irc')
-				->set('message', sprintf('<- [%2$s] <%1$s NOTICE>  %3$s', $event->get('hostmask')->getNick(), $event->get('target'), $event->get('text'))));
-		});
-
-		// Display channel happenings.
-		$dispatcher->register('irc.input.join', function(Event $event) use($dispatcher) {
-			$dispatcher->trigger(Event::newEvent('ui.message.irc')
-				->set('message', sprintf('<- %1$s (%2$s@%3$s) has joined %4$s', $event->get('hostmask')->getNick(), $event->get('hostmask')->getUsername(), $event->get('hostmask')->getHost(), $event->get('channel'))));
-		});
-		$dispatcher->register('irc.input.part', function(Event $event) use($dispatcher) {
-			if($event->get('reason') !== NULL)
-			{
-				$dispatcher->trigger(Event::newEvent('ui.message.irc')
-					->set('message', sprintf('<- %1$s (%2$s@%3$s) has left %4$s [Reason: %5$s]', $event->get('hostmask')->getNick(), $event->get('hostmask')->getUsername(), $event->get('hostmask')->getHost(), $event->get('channel'), $event->get('reason'))));
-			}
-			else
-			{
-				$dispatcher->trigger(Event::newEvent('ui.message.irc')
-					->set('message', sprintf('<- %1$s (%2$s@%3$s) has left %4$s', $event->get('hostmask')->getNick(), $event->get('hostmask')->getUsername(), $event->get('hostmask')->getHost(), $event->get('channel'))));
-			}
-		});
-		$dispatcher->register('irc.input.kick', function(Event $event) use($dispatcher) {
-			if($event->get('reason') !== NULL)
-			{
-				$dispatcher->trigger(Event::newEvent('ui.message.irc')
-					->set('message', sprintf('<- %1$s kicked %2$s %3$s [Reason: %4$s]', $event->get('hostmask')->getNick(), $event->get('user'), $event->get('channel'), $event->get('reason'))));
-			}
-			else
-			{
-				$dispatcher->trigger(Event::newEvent('ui.message.irc')
-					->set('message', sprintf('<- %1$s kicked %2$s from %3$s', $event->get('hostmask')->getNick(), $event->get('user'), $event->get('channel'))));
-			}
-		});
-		$dispatcher->register('irc.input.quit', function(Event $event) use($dispatcher) {
-			if(!$event->exists('args') || $event->get('args') === NULL)
-			{
-				$dispatcher->trigger(Event::newEvent('ui.message.irc')
-					->set('message', sprintf('<- %1$s (%2$s@%3$s) has quit [Reason: %4$s]', $event->get('hostmask')->getNick(), $event->get('hostmask')->getUsername(), $event->get('hostmask')->getHost(), $event->get('reason'))));
-			}
-			else
-			{
-				$dispatcher->trigger(Event::newEvent('ui.message.irc')
-					->set('message', sprintf('<- %1$s (%2$s@%3$s) has quit', $event->get('hostmask')->getNick(), $event->get('hostmask')->getUsername(), $event->get('hostmask')->getHost())));
-			}
-		});
-
-		// Display CTCP requests and replies
-		$dispatcher->register('irc.input.ctcp', function(Event $event) use($dispatcher) {
-			if(!$event->exists('args') || $event->get('args') === NULL)
-			{
-				$dispatcher->trigger(Event::newEvent('ui.message.irc')
-					->set('message', sprintf('<- <%1$s> CTCP %2$s - %3$s', $event->get('hostmask')->getNick(), $event->get('command'), $event->get('args'))));
-			}
-			else
-			{
-				$dispatcher->trigger(Event::newEvent('ui.message.irc')
-					->set('message', sprintf('<- <%1$s> CTCP %2$s', $event->get('hostmask')->getNick(), $event->get('command'))));
-			}
-		});
-		$dispatcher->register('irc.input.ctcp_reply', function(Event $event) use($dispatcher) {
-			if(!$event->exists('args') || $event->get('args') === NULL)
-			{
-				$dispatcher->trigger(Event::newEvent('ui.message.irc')
-					->set('message', sprintf('<- <%1$s> CTCP-REPLY %2$s - %3$s', $event->get('hostmask')->getNick(), $event->get('command'), $event->get('args'))));
-			}
-			else
-			{
-				$dispatcher->trigger(Event::newEvent('ui.message.irc')
-					->set('message', sprintf('<- <%1$s> CTCP-REPLY %2$s', $event->get('hostmask')->getNick(), $event->get('command'))));
-			}
-		});
-
-		// Display our responses
-		$dispatcher->register('runtime.postdispatch', function(Event $event) use($dispatcher) {
-			$response = $event->get('event');
-			switch($response->getName())
-			{
-				case 'irc.output.action':
-					$dispatcher->trigger(Event::newEvent('ui.message.irc')
-						->set('message', sprintf('-> [%1$s] *** %2$s', $response->get('target'), $response->get('text'))));
-				break;
-
-				case 'irc.output.ctcp':
-					if($response->exists('args') && $response->get('args') !== NULL)
-					{
-						$dispatcher->trigger(Event::newEvent('ui.message.irc')
-							->set('message', sprintf('-> [%1$s] CTCP %2$s - %3$s', $response->get('target'), $response->get('command'), $response->get('args'))));
-					}
-					else
-					{
-						$dispatcher->trigger(Event::newEvent('ui.message.irc')
-							->set('message', sprintf('-> [%1$s] CTCP %2$s', $response->get('target'), $response->get('command'))));
-					}
-				break;
-
-				case 'irc.output.ctcp_reply':
-					if($response->exists('args') && $response->get('args') !== NULL)
-					{
-						$dispatcher->trigger(Event::newEvent('ui.message.irc')
-							->set('message', sprintf('-> [%1$s] CTCP-REPLY %2$s - %3$s', $response->get('target'), $response->get('command'), $response->get('args'))));
-					}
-					else
-					{
-						$dispatcher->trigger(Event::newEvent('ui.message.irc')
-							->set('message', sprintf('-> [%1$s] CTCP-REPLY %2$s', $response->get('target'), $response->get('command'))));
-					}
-				break;
-
-				case 'irc.output.privmsg':
-					$dispatcher->trigger(Event::newEvent('ui.message.irc')
-						->set('message', sprintf('-> [%1$s] %2$s', $response->get('target'), $response->get('text'))));
-				break;
-
-				case 'irc.output.notice':
-					$dispatcher->trigger(Event::newEvent('ui.message.irc')
-						->set('message', sprintf('-> [%1$s NOTICE] %2$s', $response->get('target'), $response->get('text'))));
-				break;
-
-				default:
-					return NULL;
-				break;
-			}
-		});
-
 		return $this;
 	}
 
@@ -365,7 +229,7 @@ class Display
 
 		$this->output(self::OUTPUT_NORMAL, 'STATUS', '===================================================================');
 		$this->output(self::OUTPUT_NORMAL, 'STATUS', '');
-		$this->output(self::OUTPUT_NORMAL, 'STATUS', '  Yukari IRC Bot');
+		$this->output(self::OUTPUT_NORMAL, 'STATUS', '  Yukari');
 		$this->output(self::OUTPUT_NORMAL, 'STATUS', '-------------------------------------------------------------------');
 		$this->output(self::OUTPUT_NORMAL, 'STATUS', '@version:      %s', array(Kernel::getBuildNumber()));
 		$this->output(self::OUTPUT_NORMAL, 'STATUS', '@copyright:    (c) 2009 - 2011 Damian Bushong');
@@ -404,16 +268,6 @@ class Display
 		$this->output(self::OUTPUT_NORMAL, 'STATUS', '-------------------------------------------------------------------');
 		$this->output(self::OUTPUT_NORMAL, 'STATUS', '%s', $language->getEntry('DISPLAY_SHUTDOWN'));
 		$this->output(self::OUTPUT_NORMAL, 'STATUS', '-------------------------------------------------------------------');
-	}
-
-	/**
-	 * Method called on message being received/sent
-	 * @param \OpenFlame\Framework\Event\Instance $event - The event that is triggering the output.
-	 * @return void
-	 */
-	public function displayIRC(Event $event)
-	{
-		$this->output(self::OUTPUT_NORMAL, '', '[irc] %s', $event->get('message'));
 	}
 
 	/**
