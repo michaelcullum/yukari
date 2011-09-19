bb024874c7baa375fb1593b7fc98f8c9c64ca3c3
damianb <damian@emberlabs.org>
2011-09-19 14:37:17 -0500

    irc addon metadata file, work in progress

---
+++ addons/irc/Codebite/Yukari/Addon/Metadata/Irc.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ *
+ *===================================================================
+ *
+ *  Yukari
+ *-------------------------------------------------------------------
+ * @category    Yukari
+ * @package     addon
+ * @author      Damian Bushong
+ * @copyright   (c) 2009 - 2011 -- Damian Bushong
+ * @license     MIT License
+ * @link        https://github.com/damianb/yukari
+ *
+ *===================================================================
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this package in the file LICENSE.
+ *
+ */
+
+namespace Codebite\Yukari\Addon\Metadata;
+use Codebite\Yukari\Kernel;
+use \OpenFlame\Framework\Event\Instance as Event;
+
+/**
+ * Yukari - Addon metadata object,
+ *      Provides some information regarding the addon.
+ *
+ *
+ * @category    Yukari
+ * @package     addon
+ * @author      Damian Bushong
+ * @license     MIT License
+ * @link        https://github.com/damianb/yukari
+ */
+class IRC extends \Codebite\Yukari\Addon\Metadata\MetadataBase
+{
+	/**
+	 * @var string - The addon's version.
+	 */
+	protected $version = 'core';
+
+	/**
+	 * @var string - The addon's author information.
+	 */
+	protected $author = 'Damian Bushong';
+
+	/**
+	 * @var string - The addon's name.
+	 */
+	protected $name = 'IRC';
+
+	/**
+	 * @var string - The addon's description.
+	 */
+	protected $description = 'Provides basic support for the IRC protocol.';
+
+	/**
+	 * Hooking method for addon metadata objects, called to initialize the addon after the dependency check has been passed.
+	 * @return void
+	 */
+	public function initialize()
+	{
+		$dispatcher = Kernel::get('dispatcher');
+
+		// asdf
+	}
+
+	/**
+	 * Hooking method for addon metadata objects for executing own code on pre-load dependency check.
+	 * @return boolean - Does the addon pass the dependency check?
+	 */
+	public function checkDependencies()
+	{
+		// This addon has no dependencies.
+		return true;
+	}
+}
