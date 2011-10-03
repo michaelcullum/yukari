9ff2122626f1851128c2334c221b049827c108d7
damianb <damian@emberlabs.org>
2011-10-02 23:10:15 -0500

    the new managerstack object >.>

---
+++ addons/irc/Codebite/Yukari/Addon/IRC/ManagerStack.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ *
+ *===================================================================
+ *
+ *  Yukari
+ *-------------------------------------------------------------------
+ * @category    Yukari
+ * @package     connection
+ * @author      Damian Bushong
+ * @copyright   (c) 2009 - 2011 Damian Bushong
+ * @license     MIT License
+ * @link        https://github.com/damianb/yukari
+ *
+ *===================================================================
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this package in the file LICENSE.
+ *
+ */
+
+namespace Codebite\Yukari\Addon\IRC;
+use Codebite\Yukari\Kernel;
+use \OpenFlame\Framework\Event\Instance as Event;
+
+/**
+ * Yukari - Connection manager class,
+ * 	    Used as the IRC connection instance manager.
+ *
+ *
+ * @category    Yukari
+ * @package     connection
+ * @author      Damian Bushong
+ * @license     MIT License
+ * @link        https://github.com/damianb/yukari
+ */
+class ManagerStack
+{
+	protected $managers = array();
+
+	public function __construct($networks)
+	{
+		foreach($networks as $network => $properties)
+		{
+			$manager = new \Codebite\Yukari\Addon\IRC\Manager($network);
+
+			foreach($properties as $property => $value)
+			{
+				$manager->set($property, $value);
+			}
+
+			$this->managers[$network] = $manager;
+		}
+	}
+
+	public function tick(Event $tick)
+	{
+		foreach($this->managers as $k => $manager)
+		{
+			try
+			{
+				$manager->tickHook($tick);
+			}
+			catch(DeadConnectionException $e)
+			{
+				unset($this->managers[$k]);
+			}
+		}
+	}
+}
