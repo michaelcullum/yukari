b4474440aa9e80a37aa8fc6cd6a53c65b0249501
damianb <damian@emberlabs.org>
2011-10-10 04:13:43 -0500

    in-progress work on logging irc

---
+++ addons/logger/Codebite/Yukari/Addon/IRC/Logger.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ *
+ *===================================================================
+ *
+ *  Yukari
+ *-------------------------------------------------------------------
+ * @category    Yukari
+ * @package     addon
+ * @author      Damian Bushong
+ * @copyright   (c) 2009 - 2011 Damian Bushong
+ * @license     MIT License
+ * @link        https://github.com/damianb/yukari
+ *
+ *===================================================================
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this package in the file LICENSE.
+ *
+ */
+
+namespace Codebite\Yukari\Addon\IRC;
+use \Codebite\Yukari\Kernel;
+use \OpenFlame\Framework\Event\Instance as Event;
+
+
+/**
+ * Yukari - IRC logging class,
+ * 	    Used to log IRC going-ons.
+ *
+ *
+ * @category    Yukari
+ * @package     addon
+ * @author      Damian Bushong
+ * @license     MIT License
+ * @link        https://github.com/damianb/yukari
+ */
+class Logger
+{
+	// asdf
+}
