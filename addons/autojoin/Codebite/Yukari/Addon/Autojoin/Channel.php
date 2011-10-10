686dbdd74c61ef85755ca72ea0c7da0e343e1706
damianb <damian@emberlabs.org>
2011-10-10 13:11:20 -0500

    new autojoin addon

---
+++ addons/autojoin/Codebite/Yukari/Addon/Autojoin/Channel.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ *
+ *===================================================================
+ *
+ *  Yukari
+ *-------------------------------------------------------------------
+ * @category    Yukari
+ * @package     addon
+ * @author      Damian Bushong
+ * @copyright   (c) 2009 - 2011 Damian Bushong
+ * @license     MIT License
+ * @link        https://github.com/damianb/yukari
+ *
+ *===================================================================
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this package in the file LICENSE.
+ *
+ */
+
+namespace Codebite\Yukari\Addon\Autojoin;
+use Codebite\Yukari\Kernel;
+use \OpenFlame\Framework\Event\Instance as Event;
+
+/**
+ * Yukari - Autojoin-er object,
+ *      Automatically joins channels on invite or on end of MOTD.
+ *
+ *
+ * @category    Yukari
+ * @package     addon
+ * @author      Damian Bushong
+ * @license     MIT License
+ * @link        https://github.com/damianb/yukari
+ */
+class Channel
+{
+	public function registerListeners()
+	{
+		Kernel::registerListener('irc.input.invite', 0, array($this, 'handleInvite'));
+		Kernel::registerListener('irc.input.response.RPL_ENDOFMOTD', 0, array($this, 'handleEndOfMOTD'));
+	}
+
+	public function handleInvite(Event $event)
+	{
+		$results = array();
+
+		if(Kernel::getConfig('irc.join_invite'))
+		{
+			if(substr($event->get('text'), 0, 1) !== '#')
+			{
+				$result = Event::newEvent('irc.output.privmsg')
+					->set('target', $event->get('target'))
+					->set('text', 'Invalid channel specified.');
+			}
+			else
+			{
+				$result = Event::newEvent('irc.output.join')
+					->set('channel', $event->get('channel'));
+			}
+		}
+		else
+		{
+			$result = Event::newEvent('irc.output.privmsg')
+				->set('target', $event->get('target'))
+				->set('text', 'I\'m not allowed to join channels on invite, sorry.');
+		}
+
+		return $results;
+	}
+
+	public function handleEndOfMOTD(Event $event)
+	{
+		$network = $event->get('event')->get('network');
+		$channels = Kernel::get('irc.stack')->getNetworkOption('network', 'autojoin');
+
+		if(!$channels)
+		{
+			return;
+		}
+
+		$joins = array();
+		foreach($channels as $channel)
+		{
+			$joins[] = Event::newEvent('irc.output.join')
+				->set('channel', $channel);
+		}
+
+		return $joins;
+	}
+}
---
+++ addons/autojoin/emberlabs/materia/Metadata/Autojoin.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ *
+ *===================================================================
+ *
+ *  Yukari
+ *-------------------------------------------------------------------
+ * @category    Yukari
+ * @package     addon
+ * @author      Damian Bushong
+ * @copyright   (c) 2009 - 2011 Damian Bushong
+ * @license     MIT License
+ * @link        https://github.com/damianb/yukari
+ *
+ *===================================================================
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this package in the file LICENSE.
+ *
+ */
+
+namespace emberlabs\materia\Metadata;
+use Codebite\Yukari\Kernel;
+
+/**
+ * Yukari - Addon metadata object,
+ *      Provides some information regarding the addon.
+ *
+ *
+ * @category    Yukari
+ * @package     addon
+ * @author      Damian Bushong
+ * @license     MIT License
+ * @link        https://github.com/damianb/yukari
+ */
+class Autojoin extends \emberlabs\materia\Metadata\MetadataBase
+{
+	/**
+	 * @var string - The addon's version.
+	 */
+	protected $version = 'core';
+
+	/**
+	 * @var string - The addon's author information.
+	 */
+	protected $author = 'Damian Bushong';
+
+	/**
+	 * @var string - The addon's name.
+	 */
+	protected $name = 'Autojoin';
+
+	/**
+	 * @var string - The addon's description.
+	 */
+	protected $description = 'Automatically joins channels on end of server MOTD or on invite (if enabled)';
+
+	/**
+	 * Hooking method for addon metadata objects, called to initialize the addon after the dependency check has been passed.
+	 * @return void
+	 */
+	public function initialize()
+	{
+		// Kernel::getConfig('irc.join_invite')
+
+		$autojoin = Kernel::set('irc.addon.autojoin', new \Codebite\Yukari\Addon\Autojoin\Channel());
+		$autojoin->registerListeners();
+	}
+
+	/**
+	 * Hooking method for addon metadata objects for executing own code on pre-load dependency check.
+	 * @return boolean - Does the addon pass the dependency check?
+	 */
+	public function checkDependencies()
+	{
+		$this->loadAddonDependency('irc.stack', 'irc');
+		$this->loadAddonDependency('irc.addon.commander', 'commander');
+
+		return true;
+	}
+}
