0ec59c46f81a2214998ac078feac8c51ed10dfce
damianb <damian@emberlabs.org>
2011-10-10 05:29:49 -0500

    irc logger progress

--- addons/logger/Codebite/Yukari/Addon/IRC/Logger.php
+++ addons/logger/Codebite/Yukari/Addon/IRC/Logger.php
@@ -37,5 +37,97 @@ use \OpenFlame\Framework\Event\Instance as Event;
  */
 class Logger
 {
-	// asdf
+	public function registerListeners()
+	{
+		Kernel::registerListener('irc.input.action', -5, array($this, 'handleIRCEvent'));
+		Kernel::registerListener('irc.input.privmsg', -5, array($this, 'handleIRCEvent'));
+		Kernel::registerListener('irc.input.notice', -5, array($this, 'handleIRCEvent'));
+		Kernel::registerListener('irc.input.topic', -5, array($this, 'handleIRCEvent'));
+		Kernel::registerListener('irc.input.ctcp', -5, array($this, 'handleIRCEvent'));
+		Kernel::registerListener('irc.input.ctcp_reply', -5, array($this, 'handleIRCEvent'));
+		Kernel::registerListener('irc.input.invite', -5, array($this, 'handleIRCEvent'));
+		Kernel::registerListener('irc.input.join', -5, array($this, 'handleIRCEvent'));
+		Kernel::registerListener('irc.input.kick', -5, array($this, 'handleIRCEvent'));
+		Kernel::registerListener('irc.input.mode', -5, array($this, 'handleIRCEvent'));
+		Kernel::registerListener('irc.input.nick', -5, array($this, 'handleIRCEvent'));
+		Kernel::registerListener('irc.input.part', -5, array($this, 'handleIRCEvent'));
+		Kernel::registerListener('irc.input.quit', -5, array($this, 'handleIRCEvent'));
+	}
+
+	public function handleIRCEvent(Event $event)
+	{
+		$logger = Kernel::get('db.logger');
+
+		$_explode = explode('.', $event->getName());
+		$type = array_pop($_explode);
+		$source = $event->get('network') . ' ' . (string) $event->get('hostmask');
+		$destination = '';
+		$data = array();
+		switch($type)
+		{
+			case 'action':
+			case 'privmsg':
+			case 'notice':
+			case 'topic':
+				$destination = $event->get('target');
+				$data = array(
+					'text'		=> $event->get('text'),
+				);
+			break;
+
+			case 'ctcp':
+			case 'ctcp_reply':
+				$destination = $event->get('target');
+				$data = array(
+					'command'	=> $event->get('command'),
+					'args'		=> $event->get('args'),
+				);
+			break;
+
+			case 'invite':
+			case 'join':
+				$destination = $event->get('target');
+				$data = array(
+					'channel'	=> $event->get('channel'),
+				);
+			break;
+
+			case 'kick':
+				$destination = $event->get('channel');
+				$data = array(
+					'user'		=> $event->get('user'),
+					'reason'	=> $event->get('reason'),
+				);
+			break;
+
+			case 'mode':
+				$destination = $event->get('target');
+				$data = array(
+					'flags'		=> $event->get('flags'),
+					'args'		=> $event->get('args'),
+				);
+			break;
+
+			case 'nick':
+				$data = array(
+					'nick'		=> $event->get('nick'),
+				);
+			break;
+
+			case 'part':
+				$destination = $event->get('channel');
+				$data = array(
+					'reason'	=> $event->get('reason'),
+				);
+			break;
+
+			case 'quit':
+				$data = array(
+					'reason'	=> $event->get('reason'),
+				);
+			break;
+		}
+
+		$logger->newLogEntry($event->getName(), $source, $destination, $data);
+	}
 }
